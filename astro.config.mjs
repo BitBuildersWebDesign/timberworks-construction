2be4f800c55931df2f51e8094328787e912ee908
Parker Piedmont <pjpiedmont@gmail.com>
2025-03-18 21:33:29 -0700

    Started building navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  image: {
+    // Used for all Markdown images; not configurable per-image
+    // Used for all `<Image />` and `<Picture />` components unless overridden with a prop
+    experimentalLayout: 'responsive',
+  },
+  experimental: {
+    responsiveImages: true,
+  },
+});
